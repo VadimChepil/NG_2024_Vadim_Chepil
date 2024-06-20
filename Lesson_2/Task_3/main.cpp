9c10b0e8249b122d522fb4a1fcf959c70d84103b
Vadim <vadimvamtf@gmail.com>
2024-06-20 13:31:32 +0300

    Changed the output of the result without saving the result in Lesson_2: Task_3

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -3,34 +3,26 @@
 using namespace std;
 
 int main() {
-    const int ROW = 7; // Number of lines (maximum column length)
-    const int COL = 5; // Number of columns (maximum row length)
-    char data[ROW][COL] = {0};
-    int value[ROW] = {0};
-    for (int col = 0; col < COL; col++)
+    int value[5] = {0};
+    for (int col = 0; col < 5; col++)
     {
         // Changed the filling interface to a better version
         cout << "Enter length in " << (col + 1) << " column: ";
         cin >> value[col];
-        for (int row = 0; row < ROW; row++)
+    }
+    cout << "*****************RESULT*********************" << endl;
+    for (int col = 0; col < 7; col++)
+    {
+        for (int row = 0; row < 5; row++)
         {
-            if (value[col] > 0)
+            if (value[row] > col)
             {
-                data[row][col] = '*';
+                cout << '*';
             }
             else
             {
-                data[row][col] = ' ';
+                cout << ' ';
             }
-            value[col]--;
-        }
-    }
-    cout << "*****************RESULT*********************" << endl;
-    for (int row = 0; row < ROW; row++)
-    {
-        for (int col = 0; col < COL; col++)
-        {
-            cout << data[row][col] ;
         }
         cout << endl;
     }
