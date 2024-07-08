9df115aa503c4d3e67aa526901ffb2d54cf57f4b
Vadim <vadimvamtf@gmail.com>
2024-07-08 11:40:18 +0300

    Improved root resolution

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-    int num1 = -1;
-    int num2 = -1;
-    int res = 0;
+    float num1 = -1;
+    float num2 = -1;
+    float res = 0;
     char action = '\0';
     cout << "Select an action (+,-,*,/,^,s(sqrt)): ";
     cin >> action;
@@ -14,25 +14,26 @@ int main()
     cin >> num1;
     cout << "Enter second number: ";
     cin >> num2;
-    switch (action) {
-        case '+':
-            res = num1 + num2;
-            break;
-        case '-':
-            res = num1 - num2;
-            break;
-        case '*':
-            res = num1 * num2;
-            break;
-        case '/':
-            res = num1 / num2;
-            break;
-        case '^':
-            res = pow(num1,num2);
-            break;
-        case 's':
-            res = sqrt(num1 + num2);
-            break;
+    switch (action)
+    {
+    case '+':
+        res = num1 + num2;
+        break;
+    case '-':
+        res = num1 - num2;
+        break;
+    case '*':
+        res = num1 * num2;
+        break;
+    case '/':
+        res = num1 / num2;
+        break;
+    case '^':
+        res = pow(num1,num2);
+        break;
+    case 's':
+        res = pow(num1,(1/num2));
+        break;
     }
     cout << "Result: " << res << endl;
     return 0;
